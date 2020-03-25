cd1d59376f0a5ce72a6b0e4a0c832f8f55395ace
Denis Salem <denissalem@tuxfamily.org>
2020-03-26 00:11:21 +0100

    fix tiny rendering glitch in slider when scrolling with mouse wheel

--- src/window_management.c
+++ src/window_management.c
@@ -272,11 +272,11 @@ void listen_events() {
                 switch(x_event.xbutton.button) {
                     case Button4:
                         scroll(g_active_interactive_list, -1);
-                        break;
+                        return;
                         
                     case Button5:
                         scroll(g_active_interactive_list, 1);
-                        break;
+                        return;
                     default:
                         break;
                 }
